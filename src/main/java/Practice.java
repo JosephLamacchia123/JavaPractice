aadf6b9c732d1ec40b76025948394501b1a1a6df
JosephLamacchia123 <josephlamacchia1@gmail.com>
2023-07-28 19:41:56 -0400

    Adding some sample code

--- src/main/java/Practice.java
+++ src/main/java/Practice.java
@@ -2,5 +2,20 @@ public class Practice {
 
     public static void main(String[] args){
         System.out.println("Hello World");
+
+        for (int i = 0;i < 5; i++) {
+            System.out.println("Index :: " + i);
+        }
+
+        for (int i = 0;i < 5; i++) {
+
+            if (i == 2) {
+                System.out.println("True for Index :: " + i);
+            }
+            else {
+                System.out.println("False for Index :: " + i);
+            }
+        }
+
     }
 }
